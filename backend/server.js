e43e8530f6c13eaba462261cde37a142d23e9ac0
girirajlovevanshi <girirajlovewanshi@gmail.com>
2024-11-24 00:09:39 +0530

    server setup and db connetion done

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/db.js';
+import testRoutes from './routes/testRoutes.js';
+
+// env varaible loading
+dotenv.config({path:'./config/.env'})
+
+//initializing the app
+const app = express();
+
+//Middlewares
+app.use(express.json())
+app.use(cors())
+
+// DB connection
+connectDB();
+
+//Routes
+app.use('/api/test', testRoutes)
+
+// Starting server
+const PORT = process.env.PORT
+app.listen(PORT, ()=>{
+    console.log(`Server is listing on PORT: ${PORT}`);
+    
+})
