5394b214bb90a2b3ebefecaf1226e68af85be29b
girirajlovevanshi <girirajlovewanshi@gmail.com>
2024-11-23 10:16:43 +0530

    router for reviewer,  testing not done yet

--- Approval-system-backend/routers/application.js
+++ Approval-system-backend/routers/application.js
@@ -1,11 +1,14 @@
 import express from 'express';
-import { createApplication, getApplicationById } from '../controllers/applicationController.js';
+import { createApplication, getApplicationById, reviewApplication } from '../controllers/applicationController.js';
 import { protect, authorize } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/', protect, authorize('initiator'), createApplication);
 
-router.get('/:id', protect, getApplicationById);
+router.get('/:id', protect, getApplicationById);// for get application details
+
+router.patch('/:id/review', protect, authorize('reviewer'), reviewApplication); // for review application , only reviewr can access
+
 
 export default router;
